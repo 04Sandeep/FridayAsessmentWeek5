8e4d042c48cf14757050a9a514d1c12dd1c04fe7
04Sandeep <sandeep.kaur@infostride.com>
2022-09-09 19:09:35 +0530

    Login and Search for menu item.

--- src/test/java/NopCommerce/Search.java
+++ src/test/java/NopCommerce/Search.java
@@ -2,7 +2,6 @@ package NopCommerce;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,16 +11,15 @@ public class Search {
     WebDriver driver;
     WebDriverWait wait;
     By search = By.xpath("//input[@class = 'form-control admin-search-box typeahead tt-input']");
-
     public Search(WebDriver driver)
     {
         this.driver = driver;
     }
-
     public void searchInfo()
     {
         driver.findElement(search).click();
-
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.elementToBeClickable(search));
         driver.findElement(search).sendKeys("Dashboard");
     }
 }
